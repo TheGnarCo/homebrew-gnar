b194fe917e8152b5cc2a54b4342b3e714974158b
gnarbot <gnarbot@thegnar.co>
2022-05-06 20:05:58 +0000

    Update from https://github.com/TheGnarCo/gnar-cli/commit/6c2ae886c7956633e9f3bdfc5ec2f8608ac37d90

--- Formula/gnar.rb
+++ Formula/gnar.rb
@@ -1,12 +1,14 @@
+# Generated from Github Workflow
+# See https://github.com/TheGnarCo/gnar-cli for more information
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class Gnar < Formula
   desc 'Decorate your Apps with Gnarly opinions'
   homepage 'https://github.com/TheGnarCo/gnar-cli'
-  url 'https://github.com/TheGnarCo/gnar-cli/releases/download/v0.0.11/gnar.tar.gz'
-  sha256 '89720e63cefa7859d16d34d83ed7d03a8837ba67a324ebda3a8794388748db72'
+  url 'https://github.com/TheGnarCo/gnar-cli/releases/download/v0.0.12/gnar.tar.gz'
+  sha256 '5a2620e7c037042c6225eaf2cc95f66976848cfb669e91c1c3837013b11ccd7e'
   license 'MIT'
-  version '0.0.11'
+  version '0.0.12'
 
   def install
     bin.install 'gnar'
